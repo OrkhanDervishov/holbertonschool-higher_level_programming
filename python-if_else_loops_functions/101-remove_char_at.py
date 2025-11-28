ce3121941447420af2ada622af5b47b8be3a1ad5
OrkhanDervishov <dwarfweird@gmail.com>
2025-11-28 16:32:49 +0400

    task 14

---
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    if n > len(str) or n < 0:
+        return str
+    char_list = list(str)
+    char_list[n] = ''
+    return "".join(char_list)
+
+print(remove_char_at("Best School", 3))
+print(remove_char_at("Chicago", 2))
+print(remove_char_at("C is fun!", 0))
+print(remove_char_at("School", 10))
+print(remove_char_at("Python", -2))
