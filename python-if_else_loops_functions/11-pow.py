a7dac5edc949ec5b962c72ea9b0265ec36c6dd88
OrkhanDervishov <dwarfweird@gmail.com>
2025-11-28 15:55:42 +0400

    task 12

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 def pow(a, b):
-    power = a
-    for i in range(b-1):
-        power *= a
+    if b > 0:
+        power = a
+        for i in range(b-1):
+            power *= a
+    elif b < 0:
+        power = 1/a
+        for i in range((-b)-1):
+            power /= a
+    else:
+        power = 1
     return power
+
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))
