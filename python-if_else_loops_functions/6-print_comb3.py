f075f54489a804b455bd5776c5caa0243a5934cf
OrkhanDervishov <dwarfweird@gmail.com>
2025-11-28 15:14:05 +0400

    task 7

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 9):
-    for j in range(1 + (i*11), 1 + (i*11) + 9 - i):
-        if(i != 8):
-            print("{num:02d}".format(num=j), end=", ")
-        else:
-            print("{num:02d}".format(num=j))
+for i in range(8):
+    for j in range(i+1, 10):
+        print("{0}{1}".format(i, j), end=", ")
+
+print(89)
