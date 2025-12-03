50b1c56536889d9e2eaeed1efc67954b1bef1f5a
OrkhanDervishov <dwarfweird@gmail.com>
2025-12-03 12:17:42 +0400

    fix task 1

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,4 +4,4 @@ from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{va} + {vb} = {res}".format(va = a, vb = b, res = add(a, b)))
+    print("{va} + {vb} = {res}".format(va=a, vb=b, res=add(a, b)))
