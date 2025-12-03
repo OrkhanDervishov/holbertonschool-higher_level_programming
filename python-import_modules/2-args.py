604d4fbca9bb437db8b1f7233aa879d59207ab1e
OrkhanDervishov <dwarfweird@gmail.com>
2025-12-03 12:52:39 +0400

    fix task 3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,6 +2,9 @@
 import sys
 
 if __name__ == "__main__":
-    print("{} argument:".format(len(sys.argv) - 1))
+    if (len(sys.argv) - 1) == 1:
+        print("{} argument:".format(len(sys.argv) - 1))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
         print("{num}: {arg}".format(num=i, arg=sys.argv[i]))
