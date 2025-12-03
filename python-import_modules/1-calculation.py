585e7ee1f1542a334bf3cb78548d55e79f2c42f9
OrkhanDervishov <dwarfweird@gmail.com>
2025-12-03 12:37:07 +0400

    task 2

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import add, sub, mul, div from calculator-1
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{va} + {vb} = {res}".format(va=a, vb=b, res=add(a, b)), end="\n")
+    print("{va} - {vb} = {res}".format(va=a, vb=b, res=sub(a, b)), end="\n")
+    print("{va} * {vb} = {res}".format(va=a, vb=b, res=mul(a, b)), end="\n")
+    print("{va} / {vb} = {res}".format(va=a, vb=b, res=div(a, b)), end="\n")
