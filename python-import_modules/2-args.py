c053b8f8afdb9916fc8eb27eebed24325ee530dd
OrkhanDervishov <dwarfweird@gmail.com>
2025-12-03 12:55:44 +0400

    fix task 3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 
 if __name__ == "__main__":
     if (len(sys.argv) - 1) == 0:
-        print("{} argument.".format(len(sys.argv) - 1))
+        print("{} arguments.".format(len(sys.argv) - 1))
     elif (len(sys.argv) - 1) == 1:
         print("{} argument:".format(len(sys.argv) - 1))
     else:
