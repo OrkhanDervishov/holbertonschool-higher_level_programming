cc2dbae237b8ba45bc2720db7f362652ca791425
OrkhanDervishov <dwarfweird@gmail.com>
2025-12-10 14:54:55 +0400

    task 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -7,8 +7,3 @@ def uniq_add(my_list=[]):
             new_list.append(i)
             sum += i
     return sum
-
-my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
-print("Result: {:d}".format(result))
-    
