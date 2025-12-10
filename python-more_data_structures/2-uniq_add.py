ca5170d51f5d16ea18147e29fdb72d1d0503385b
OrkhanDervishov <dwarfweird@gmail.com>
2025-12-10 14:54:24 +0400

    task 2

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list = []
+    sum = 0
+    for i in my_list:
+        if i not in new_list:
+            new_list.append(i)
+            sum += i
+    return sum
+
+my_list = [1, 2, 3, 1, 4, 2, 5]
+result = uniq_add(my_list)
+print("Result: {:d}".format(result))
+    
