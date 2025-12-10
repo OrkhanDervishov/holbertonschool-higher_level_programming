51a4de63430d5baa0147efa8915a18f8588cbcc8
OrkhanDervishov <dwarfweird@gmail.com>
2025-12-10 14:40:14 +0400

    task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,6 @@ def square_matrix_simple(matrix=[]):
     for i in matrix:
         row = []
         for j in i:
-           row.append(j*j)
+            row.append(j*j)
         square_matrix.append(row)
     return square_matrix
