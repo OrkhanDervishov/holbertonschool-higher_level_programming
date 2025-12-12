02886d3b54e90ea76584ed5a762c481d4ed34f0d
OrkhanDervishov <dwarfweird@gmail.com>
2025-12-12 15:30:21 +0400

    task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -19,20 +19,3 @@ def roman_to_int(roman_string):
         elif i == 'M':
             sum += 1000
     return sum
-
-
-            
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
