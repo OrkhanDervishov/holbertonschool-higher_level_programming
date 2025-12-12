dcedc34161d9c7fd3ed3bbf518d922ce5b699c47
OrkhanDervishov <dwarfweird@gmail.com>
2025-12-12 15:52:19 +0400

    task 1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@ def safe_print_integer(value):
     try:
         if isinstance(value. int) == False:
             raise Exception()
-        print("{}".format(value))
+        print("{:d}".format(value))
     except Exception:
-        print("{} is not integer".format(value))
+        print("{} is not an integer".format(value))
