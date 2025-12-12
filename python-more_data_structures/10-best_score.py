52ad74723b346b9360af87ef421b8fb13683593e
OrkhanDervishov <dwarfweird@gmail.com>
2025-12-12 15:02:31 +0400

    task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -8,11 +8,3 @@ def best_score(a_dictionary):
         maxKey = i if a_dictionary[i] > maxValue else maxKey
         maxValue = a_dictionary[maxKey]
     return maxKey
-
-
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
