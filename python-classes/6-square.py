8146a2b3da5f6a4fe64c7732db1f2c3b7bc89a98
OrkhanDervishov <dwarfweird@gmail.com>
2025-12-19 15:53:07 +0400

    task 6

---
+++ python-classes/6-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+'''Some Documentation'''
+
+
+class Square:
+    '''Class Documentation'''
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def position(self, position):
+        if not isinstance(position, int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
+
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
+
+    
