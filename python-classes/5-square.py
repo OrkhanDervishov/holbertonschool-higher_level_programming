a769de00863957677eac7da2b53c70df7e3dcef4
OrkhanDervishov <dwarfweird@gmail.com>
2025-12-19 15:49:05 +0400

    task 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -24,7 +24,10 @@ class Square:
         self.__size = size
 
     def my_print(self):
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print("#", end="")
+        if self.__size == 0:
             print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
