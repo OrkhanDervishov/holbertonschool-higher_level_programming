08addab6d5744f965c5ed4e0851f76dd1d03315a
OrkhanDervishov <dwarfweird@gmail.com>
2025-12-19 17:32:03 +0400

    task 1

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     '''Documentation'''
     number_of_instances = 0
-    print_symbol = #
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         self.width = width
