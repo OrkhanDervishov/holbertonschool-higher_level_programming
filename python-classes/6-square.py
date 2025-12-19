91d46c524bf66bd4d9d9fc057ae09fb86c2edf21
OrkhanDervishov <dwarfweird@gmail.com>
2025-12-19 16:16:33 +0400

    task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-'''Some Documentation'''
+"""Define a class Square."""
 
 
 class Square:
-    '''Class Documentation'''
-   def __init__(self, size=0, position=(0, 0)):
+    """Represent a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
 
         Args:
