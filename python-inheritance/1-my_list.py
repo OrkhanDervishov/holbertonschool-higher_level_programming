326ae916720833fa533583c92f3049ece2c499fb
OrkhanDervishov <dwarfweird@gmail.com>
2025-12-28 16:16:41 +0400

    task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module contains the function lookup"""
+
+
+class MyList(list):
+    """MyList class"""
+
+    def print_sorted(self):
+        """Print"""
+        print(sorted(self))
