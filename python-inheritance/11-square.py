96ae8d122510be139fba34ec4f1214eca806fee0
OrkhanDervishov <dwarfweird@gmail.com>
2025-12-28 17:07:36 +0400

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Doc"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Doc"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super.__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
