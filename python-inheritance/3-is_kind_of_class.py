130ec396d3721c2adb6f49c62d4e1db42d5a88e6
OrkhanDervishov <dwarfweird@gmail.com>
2025-12-28 16:30:32 +0400

    task 3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -4,9 +4,8 @@
 
 def is_kind_of_class(obj, a_class):
     """Doc"""
-    if obj is None:
-        return None
-    elif obj is a_class:
+    if obj is a_class:
         return True
     elif isinstance(obj, a_class):
         return True
+    return False
