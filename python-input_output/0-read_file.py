3f05ee47f7b3045abf1c68634b119ec2278d4110
OrkhanDervishov <derviorxan7@gmail.com>
2026-01-04 12:37:34 +0400

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Doc"""
 
+
 def read_file(filename=""):
     """Doc"""
     with open(filename, encoding="utf-8") as file:
-         print(file.read(), end="")
+        print(file.read(), end="")
