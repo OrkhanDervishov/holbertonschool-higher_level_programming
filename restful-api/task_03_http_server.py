862c5db793c2b776a28eda5c29db4617926e270b
OrkhanZero <derviorxan7@gmail.com>
2026-01-11 22:39:11 +0400

    task 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -39,3 +39,6 @@ def run(server_class=HTTPServer, handler_class=SimpleAPIHandler, port=8000):
     httpd = server_class(server_address, handler_class)
     print(f"Server running on http://localhost:{port}")
     httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
