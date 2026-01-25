a3587d00bd2a76d9e9b6435aa31c7ce2aa72d73a
OrkhanDervishov <derviorxan7@gmail.com>
2026-01-25 13:08:32 -0500

    tasks

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+doc
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """
+    doc
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
